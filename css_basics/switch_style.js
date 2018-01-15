0d4a7962156c59f2709c838ed4ccbea4d79c29c9
Ben Haley <bhaley7@umd.edu>
2018-01-14 22:56:08 -0500

    created 4 functions that change the stylesheet for the html document

--- css_basics/switch_style.js
+++ css_basics/switch_style.js
@@ -1,19 +1,54 @@
+/* Function is not called when button is clicked because of parameter */
 function style(x) {
-    window.alert("Here is x : " + x);
     var text = "";
+    text += "<div id=\"style\">\n";
     switch (x) {
         case 1:
-            text = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style1.css\" id=\"stylesheet\">";
+            text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style1.css\" id=\"stylesheet\">";
             break;
         case 2:
-            text = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style2.css\" id=\"stylesheet\">";
+            text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style2.css\" id=\"stylesheet\">";
             break;
         case 3:
-            text = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style3.css\" id=\"stylesheet\">";
+            text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style3.css\" id=\"stylesheet\">";
             break;
         case 4:
-            text = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style4.css\" id=\"stylesheet\">";
+            text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style4.css\" id=\"stylesheet\">";
             break;
     }
-    document.getElementsByTagName("link").innerHTML = text;
+    text += "\n</div>";
+    document.getElementsById("style").innerHTML = text;
+}
+
+
+function style1 () {
+    var text = "";
+    text += "<div id=\"style\">\n";
+    text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style1.css\">\n";
+    text += "</div>";
+    document.getElementById("style").innerHTML = text;
+}
+
+function style2 () {
+    var text = "";
+    text += "<div id=\"style\">\n";
+    text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style2.css\">\n";
+    text += "</div>";
+    document.getElementById("style").innerHTML = text;
+}
+
+function style3 () {
+    var text = "";
+    text += "<div id=\"style\">\n";
+    text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style3.css\">\n";
+    text += "</div>";
+    document.getElementById("style").innerHTML = text;
+}
+
+function style4 () {
+    var text = "";
+    text += "<div id=\"style\">\n";
+    text += "<link rel=\"stylesheet\" type=\"text/css\" href=\"style4.css\">\n";
+    text += "</div>";
+    document.getElementById("style").innerHTML = text;
 }
